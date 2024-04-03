25c8c01e3279bf95d9176a6d52d7272210520649
Mahipal012 <165192782+Mahipal012@users.noreply.github.com>
2024-04-03 13:10:14 +0000

    varible

---
+++ 01_basic/varible.js
@@ -0,0 +1,22 @@
+// var name="mahipal"
+// let age=23
+// city="jaipur"
+// const id=1011
+// // id=2343
+// name="Shivpal"
+// age=24
+// // console.log(id);
+// console.table([name,age,city])
+
+
+//data types
+ let name="Mahipal"
+ let age=23
+ let isLogin=true
+ let city //undefined
+ let dist=null
+ console.log(typeof name)
+ console.log(typeof age)
+ console.log(typeof city)
+ console.log(typeof dist)
+ console.log(typeof isLogin)
